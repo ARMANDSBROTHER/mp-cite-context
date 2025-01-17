6fc203028265b2522b1f77f73d15f4f45c42f390
ARMANDSBROTHER <167355352+ARMANDSBROTHER@users.noreply.github.com>
2025-01-17 21:14:58 +0000

    Switch to await approach, (which is better, why didnt we start with this fml)

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-const promiseOfResponse = fetch("https://dx.doi.org/10.1210/jc.2017-01658", {
-  headers: {
-    Accept: "application/json",
-  },
-});
-
-promiseOfResponse.then((successfulResponse) =>
-  successfulResponse.json().then((jsontext) => console.log(jsontext))
+const successfulResponse2 = await fetch(
+  "https://dx.doi.org/10.1210/jc.2017-01658",
+  {
+    headers: {
+      Accept: "application/json",
+    },
+  }
 );
+
+const jsontext = await successfulResponse2.json();
+
+console.log(jsontext);
+
+export {};
